5aaf106e2396c6b5d8786dbb228d63a2c489e533
rmnk-nz <rua.manukau20@gmail.com>
2021-11-22 21:25:59 +1100

    added the database models

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,37 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    },
+    ProductTag: {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'product',
+          key: 'id'
+        }
+      },
+      tag_id: {
+        type: DataTypes.INTEGER,
+        reference: {
+          model: 'tag',
+          key: 'id'
+        },
+      },
+    },
   },
   {
     sequelize,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -25,7 +25,12 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    tagIds: req.body.tagIds
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -91,6 +96,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id:req.params.id
+    }
+  })
+  .then(data => {
+    if(!data) {
+      res.status(404).json ({messsage: 'Product not found'});
+      return;
+    }
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
